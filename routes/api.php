a552ea301f49b6884e553a853c6173336500b590
Dmytro Slipak <dslp.developer@gmail.com>
2024-10-19 19:10:09 +0300

    update routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/fetch-users', 'App\Http\Controllers\DatingController@fetchUsers');
+    Route::post('/like', 'App\Http\Controllers\DatingController@like');
+    Route::post('/dislike', 'App\Http\Controllers\DatingController@dislike');
+    Route::get('/matches', 'App\Http\Controllers\DatingController@matches');
+    Route::get('/profile/{id?}', 'App\Http\Controllers\ProfileController@getProfile');
+    Route::patch('/profile', 'App\Http\Controllers\ProfileController@update');
+    Route::post('/unmatch', 'App\Http\Controllers\DatingController@unmatch');
+    Route::post('/generate-invoice', 'App\Http\Controllers\TelegramBotController@generateInvoice');
+
+
 });
+
+
+
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::post('/register', 'App\Http\Controllers\AuthController@register');
+Route::get('/app-metas', 'App\Http\Controllers\AppMetaController@getAppMetas');
+Route::get('/photos/{file_id}', 'App\Http\Controllers\PhotoController@getPhoto');
+Route::post('/telegram-hook', 'App\Http\Controllers\TelegramBotController@handleWebhook');
