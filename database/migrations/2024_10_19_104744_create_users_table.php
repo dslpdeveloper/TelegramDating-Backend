1b52265064391cb4b2e5780670e390102ba87572
root <root@secondbaserm.site>
2024-10-19 13:59:27 +0300

    add create users migration

--- database/migrations/2024_10_19_104744_create_users_table.php
+++ database/migrations/2024_10_19_104744_create_users_table.php
@@ -14,9 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email');
+            $table->timestamp('email_veified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->string('tg_id')->unique();
+            $table->integer('age');
+            $table->string('tg_username')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('photos', 1000)->nullable();
+            $table->boolean('has_telegram_premium');
+            $table->boolean('is_pro_user');
+            $table->tinyInteger('user_type');
+            $table->tinyInteger('status');
             $table->rememberToken();
             $table->timestamps();
         });
